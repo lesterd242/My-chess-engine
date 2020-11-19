a379c6f7a036210ee1b73dba0d7b864e0b0f22e1
lesterd242 <74271911+lesterd242@users.noreply.github.com>
2020-11-19 17:32:03 -0600

    Se agrega funcionalidad para imprimir tablero

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -4,7 +4,14 @@ package utils;
 import java.util.Arrays;
 
 public class Utils {
-    public static void imprimirTablero(String tablero[][]){
+    public static void imprimirTablero(String tablero[][], int opc, String movimiento){
+        
+        if(opc == 1){
+            System.out.println("movimiento deshecho " + movimiento);
+        }else{
+            System.out.println("movimiento hecho " + movimiento);
+        }
+        
         for(int x = 0; x < 8; x++){
             System.out.println(Arrays.toString(tablero[x]).replace('[', '|').replace(']', '|'));
         }
