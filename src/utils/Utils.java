f6bec2bf2f1eaf1b1926c3a72dc8c2ac86a1eafb
lesterd242 <74271911+lesterd242@users.noreply.github.com>
2020-11-25 17:34:30 -0600

    Se mejora metodo de impresion

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -6,10 +6,16 @@ import java.util.Arrays;
 public class Utils {
     public static void imprimirTablero(String tablero[][], int opc, String movimiento){
         
-        if(opc == 1){
-            System.out.println("movimiento deshecho " + movimiento);
-        }else{
-            System.out.println("movimiento hecho " + movimiento);
+        switch (opc) {
+            case 1:
+                System.out.println("movimiento deshecho " + movimiento);
+                break;
+            case 0:
+                System.out.println("movimiento hecho " + movimiento);
+                break;
+            case 2:
+                System.out.println("Se gira tablero");
+                break;
         }
         
         for(int x = 0; x < 8; x++){
