a25fa70a3e9fcd3ad963a7edb10df741d706ef6b
lesterd242 <74271911+lesterd242@users.noreply.github.com>
2021-01-26 17:51:29 -0600

    Se añade método para aleatorizar jugadas

--- src/Controllers/Pruebas.java
+++ src/Controllers/Pruebas.java
@@ -2,9 +2,64 @@ package Controllers;
 
 public class Pruebas {
     public static void main(String[] args) {
-        for (int i = -1; i < 2; i+=2) {
-            System.out.println(i);
-            
-        }
+       
     }
+    
+    private static String mezclaLista(String lista) {
+        Random rnd = new Random();
+        //System.out.println("Imprimiendo lista a desordenar " + lista);
+        String nuevaLista = "";
+
+        if (lista != null && !lista.equals("")) {
+            if (lista.length() == 5) {
+                return lista;
+            } 
+            int numeroJugadas = lista.length() / 5;
+            int nuevosIndices[] = new int[numeroJugadas];
+            String listaDividida[] = new String[numeroJugadas];
+            numeroJugadas = 0;
+            for (int i = 0; i < lista.length();) {
+                listaDividida[numeroJugadas] = lista.substring(i, i + 5);
+                numeroJugadas++;
+                i = i + 5;
+            }
+            
+            numeroJugadas = lista.length() / 5;
+            int x = 1;
+            int indiceRnd;
+            boolean bandera;
+            try {
+                while (x < numeroJugadas) {
+                    bandera = true;
+                    indiceRnd = rnd.nextInt(numeroJugadas);
+                    for (int i = 0; i < numeroJugadas; i++) {
+                        if (indiceRnd == nuevosIndices[i]) {
+                            bandera = false;
+                            if (!bandera) {
+                                break;
+                            }
+                        }
+                    }
+                    if (bandera) {
+                        nuevosIndices[x - 1] = indiceRnd;
+                        x++;
+                    }
+                }
+                
+                for (int i = 0; i < numeroJugadas; i++) {
+                    nuevaLista += listaDividida[nuevosIndices[i]];
+                }
+                
+            } catch (Exception e) {
+                System.out.println(e.toString());
+                int error = 0;
+            }
+        } else {
+            return lista;
+        }
+
+        return nuevaLista;
+    }
+    
+    
 }
