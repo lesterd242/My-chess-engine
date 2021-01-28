90a4fb311998702f5dcff06cd15295504683e98d
lesterd242 <74271911+lesterd242@users.noreply.github.com>
2021-01-28 15:59:30 -0600

    Se mejora función de evaluación

--- src/evaluations/Rating.java
+++ src/evaluations/Rating.java
@@ -1,18 +1,22 @@
 package evaluations;
 
 import Controllers.Main;
+import utils.Utils;
 
 public class Rating {
+    
     public static int rating(int list, int depth){
         int counter = 0;
         counter += rateAttack();
         counter += rateMaterial();
-        counter += rateMoveability();
+        counter += rateMoveability(list);
+        counter += checkStaleMate(list, depth);
         counter += ratePositional();
         Main.giraTablero();
         counter -= rateAttack();
         counter -= rateMaterial();
-        counter -= rateMoveability();
+        counter -= rateMoveability(list);
+        counter -= checkStaleMate(Main.generaMovimientos().length(), depth);
         counter -= ratePositional();
         Main.giraTablero();
         return -(counter+depth*50);
@@ -46,9 +50,25 @@ public class Rating {
         
         return counter;
     }
-    private static int rateMoveability(){
-        return 0;
+    private static int rateMoveability(int list) {
+        int counter = list;
+        return counter;
     }
+    
+    private static int checkStaleMate(int list, int depth){
+        
+        int counter = 0;
+        if (list == 0) {
+            if (!Main.reySeguro()) {
+                counter += -(depth == 0 ? 25000:25000*depth);
+            } else {
+                counter += -(depth == 0 ? 12000:12000*depth);
+            }
+        }
+        
+        return counter;
+    }
+    
     private static int ratePositional(){
         return 0;
     }
